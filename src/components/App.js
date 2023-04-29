f3ced3331627822b353973b4054d9316d6209e04
Eric Mugo Thiong'o <mugoericpg20@gmail.com>
2023-04-29 23:55:17 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import { Route, NavLink, Switch } from 'react-router-dom';
-import Home from './Home';
-import Movies from './Movies';
-import Directors from './Directors';
-import Actors from './Actors';
-import NavBar from './NavBar';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import Actors from "./Actors";
+import Directors from "./Directors";
+import Movies from "./Movies";
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Switch>
-        
-        <Route exact path="/" component={Home} />
-        <Route path="/movies" component={Movies} />
-        <Route path="/directors" component={Directors} />
-        <Route path="/actors" component={Actors} />
-        
-      </Switch>
-    </div>
-  );
+  return <div>
+  <NavBar />
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route path="/movies" component={Movies} />
+    <Route path="/directors" component={Directors} />
+    <Route path="/actors" component={Actors} />
+  </Switch>
+  </div>;
 }
 
 export default App;
